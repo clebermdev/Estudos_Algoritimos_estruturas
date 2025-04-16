d9237a66ca68148f146ac52f68ba822a822608d7
Cleber Moraes <assmoraes.cjc@gmail.com>
2025-04-16 11:02:02 -0300

    Polimorfismo

--- Aula04 - Herança/TelaCarro.py
+++ Aula04 - Herança/TelaCarro.py
@@ -2,11 +2,16 @@ import sys
 from PyQt5.QtWidgets import *
 from Carro import Carro
 from TelaVeiculo import TelaVeiculo
+#from TelaCategoria import TelaCategoria
 
 class TelaCarro( TelaVeiculo ):
 
-    def __init__(self, titulo = "Tela de Carro"):
+    def __init__(self, titulo = "Tela de Carro", categorias = [], telaCat = None ):
+        self.telaCategorias = telaCat
+        self.listaCategorias = categorias
+        self.telaCategorias.telaCarro = self
         super().__init__(titulo)
+        
 
     def definirLayout(self):
         super().definirLayout()
@@ -15,17 +20,41 @@ class TelaCarro( TelaVeiculo ):
         self.layout.addWidget( self.lblPortas )
         self.layout.addWidget( self.txtPortas )
 
+        self.lblCategoria = QLabel("Categoria:")
+        self.layout.addWidget( self.lblCategoria)
+
+        self.cmbCategoria = QComboBox(self)
+        self.carregarCategorias()
+
+
+        self.layout.addWidget(self.cmbCategoria )
+
+        self.btnAddCategoria = QPushButton("Adicionar Categoria", self)
+        self.btnAddCategoria.clicked.connect( self.abrirTelaCategoria )
+        self.layout.addWidget( self.btnAddCategoria)
+
+    def carregarCategorias(self):
+        self.cmbCategoria.clear()
+        self.cmbCategoria.addItem( "Selecione...", None)
+        for cat in self.listaCategorias:
+            self.cmbCategoria.addItem( cat.nome , cat)
+
+    def abrirTelaCategoria(self):
+        self.telaCategorias.show()
+
     def salvar(self):
         modelo = self.txtModelo.text() 
-        if modelo != "" :
+        if modelo != "" and self.cmbCategoria.currentIndex != 0  :
             ano = self.txtAno.text()
             valor = None
             if ano != "":
                 valor = int( ano )
 
-            portas = self.txtPortas.text()
+            portas = self.txtPortas.text() 
             vPortas = None
             if portas != "":
                 vPortas = int( portas ) 
-            carro = Carro(modelo, ano, vPortas)
+            
+            cat = self.cmbCategoria.currentData()
+            carro = Carro(modelo, ano, vPortas, cat)
             QMessageBox.information(self, "Carro Salvo", str(carro) )
--- Aula04 - Herança/main.py
+++ Aula04 - Herança/main.py
@@ -3,13 +3,21 @@ from PyQt5.QtWidgets import QApplication
 
 from TelaVeiculo import TelaVeiculo
 from TelaCarro import TelaCarro
+from TelaCategoria import TelaCategoria
+from Categoria import Categoria
 
 app = QApplication( sys.argv )
 
-telaVeiculo = TelaVeiculo( "Cadastro de Veículo")
-telaVeiculo.show()
+#telaVeiculo = TelaVeiculo( "Cadastro de Veículo")
+#telaVeiculo.show()
 
-telaCarro = TelaCarro( "Cadastro de Carro")
+categorias = []
+telaCat = TelaCategoria("Adicionar Categorias", categorias )
+#telaCat.show()
+
+telaCarro = TelaCarro( "Cadastro de Carro", categorias, telaCat)
 telaCarro.show()
+
+#telaCat.telaCarro = telaCarro
 
 sys.exit( app.exec_() )
